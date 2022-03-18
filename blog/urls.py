ef42f2c4bfff5bb453db10afe4a00856e72da73d
Rodman9110 <jacobo34@hotmail.es>
2022-03-18 21:33:27 +0100

    add form

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,13 @@ import django
 
 
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView,BlogCreateView
 
 app_name="blog"
 
 
 urlpatterns = [
-    path('',BlogListView.as_view(),name='home')
+    path('',BlogListView.as_view(),name='home'),
+    path('create/',BlogCreateView.as_view(),name='create')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from importlib.resources import contents
-from multiprocessing import context
-from django.shortcuts import render
+# from importlib.resources import contents
+# from multiprocessing import context
+from django.shortcuts import render,redirect,get_list_or_404
 from django.views.generic.base import View
-
+from .forms import PostCreateForm
+from .models import Post
 class BlogListView(View):
     def get(self , request ,*args, **kwargs):
         context={
@@ -10,4 +11,30 @@ class BlogListView(View):
         }
         return render(request,'blog_list.html',context)
 
+class BlogCreateView(View):
+    def get(self,request,*args,**kwargs):
+        form=PostCreateForm()
+        context={
+            'form':form
+        }
+        return render(request,'blog_create.html',context)
+    def post(self,request,*args,**kwargs):
+        if request.method=="POST":
+            form = PostCreateForm(request.POST)
+            # print("Post")
+            # print(form.is_valid())
+            if form.is_valid():
+                title =  form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+                # print("-----------------------------------")
+                # print(title)
+                # print(content)
+                # print("-----------------------------------")
+                p, created = Post.objects.get_or_create(title=title,content=content)
+                p.save()
+                return redirect('blog:home')
 
+        context={
+
+        }
+        return render(request,'blog_create.html',context)
